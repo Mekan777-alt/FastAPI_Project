553849cce2001d0ab23aff14e25bc5b4b0884c1e
Mekan777-alt <mekan2678@gmail.com>
2024-03-10 19:36:55 +0300

    added admin

--- api/routers/UK/objects.py
+++ api/routers/UK/objects.py
@@ -3,8 +3,10 @@ from typing import Annotated
 from starlette.responses import JSONResponse
 from config import get_session
 from starlette import status
-from firebase.config import get_firebase_user_from_token, get_user
+from firebase.config import get_firebase_user_from_token
 from sqlalchemy.ext.asyncio import AsyncSession
+from api.routers.UK.config import get_objects_from_uk
+from schemas.uk.get_objects import ObjectSchemas
 
 
 router = APIRouter(
@@ -12,16 +14,16 @@ router = APIRouter(
 )
 
 
-@router.get("/get_objects_uk")
+@router.get("/get_objects_uk", response_model=ObjectSchemas)
 async def get_objects(user: Annotated[dict, Depends(get_firebase_user_from_token)],
                       session: AsyncSession = Depends(get_session)):
 
-    staff = get_user(user, session)
-
     try:
 
-        pass
+        data = await get_objects_from_uk(session, user)
+
+        return JSONResponse(content=data, status_code=status.HTTP_200_OK)
 
     except Exception as e:
 
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=str(e))
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=str(e))
