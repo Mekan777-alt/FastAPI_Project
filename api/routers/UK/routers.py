890f8c3d3fbe35f57a577408f4f8c9915c0b08d9
Mekan777-alt <mekan2678@gmail.com>
2024-05-23 20:10:16 +0300

    update router

--- api/routers/UK/routers.py
+++ api/routers/UK/routers.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from .get_staff import router as get_staff_router
-from .UK import router as uk_router
 from .objects import router as objects_list_router
 from .get_profile_uk import router as get_profile_uk_router
 from .create_staff import router as create_staff_router
@@ -12,7 +11,6 @@ admin_router = APIRouter(
     tags=['Company']
 )
 
-admin_router.include_router(uk_router)
 admin_router.include_router(get_staff_router)
 admin_router.include_router(objects_list_router)
 admin_router.include_router(get_profile_uk_router)
