4da16b1d2ebd56548a7ec49e243018e90c1d1157
Mekan777-alt <mekan2678@gmail.com>
2024-06-11 12:57:06 +0300

    update db_data

--- db_data.py
+++ db_data.py
@@ -4,17 +4,17 @@ from config import DB_HOST, DB_NAME, DB_PORT, DB_USER, DB_PASSWORD
 import psycopg2
 
 try:
-    conn = psycopg2.connect(dbname="fastAPI", user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
 
     if conn:
         cursor = conn.cursor()
         cursor.execute("INSERT INTO uk_profiles (uuid, photo_path, name, device_token) VALUES (%s, %s, %s, %s)",
                            ('GHMBxjGo4TUMxFgSnw773qOyYdO2', None, 'AdriaApart', None))
         conn.commit()
-        cursor.execute("INSERT INTO uk_profiles (uuid, photo_path, name, device_token) VALUES (%s, %s, %s, %s)",
-                       ('hnBiZATbISeH4k9vTtTmX4sA1LK2', 'null', 'UK_NAME - 2',
-                        "dpqmvMwUST-gOWVf3KkG8P:APA91bFd8JXPRWf_VUlI7QFNBBTtdLX0_Tkbls_63csq9pbu-JAHDQBV2d6OTldOatfCAeLPxPDe1-7Byaw1gY9X7NUQG26shoU7WL_mh8HcNRmvAnbAY6b8LgYborGsSJv1NtXxXHmi"))
-        conn.commit()
+        # cursor.execute("INSERT INTO uk_profiles (uuid, photo_path, name, device_token) VALUES (%s, %s, %s, %s)",
+        #                ('hnBiZATbISeH4k9vTtTmX4sA1LK2', 'null', 'UK_NAME - 2',
+        #                 "dpqmvMwUST-gOWVf3KkG8P:APA91bFd8JXPRWf_VUlI7QFNBBTtdLX0_Tkbls_63csq9pbu-JAHDQBV2d6OTldOatfCAeLPxPDe1-7Byaw1gY9X7NUQG26shoU7WL_mh8HcNRmvAnbAY6b8LgYborGsSJv1NtXxXHmi"))
+        # conn.commit()
         # cursor.execute("INSERT INTO executor_profiles (uuid, specialization, photo_path) VALUES (%s, %s, %s)",
         #                ('uJd4bz7tRdaYq126edLI4aqLR9I3', 'Cleaning', 'null'))
         # cursor.execute("INSERT INTO executor_profiles (uuid, specialization, photo_path) VALUES (%s, %s, %s)",
@@ -26,26 +26,26 @@ try:
         # cursor.execute("INSERT INTO executor_profiles (uuid, specialization, photo_path) VALUES (%s, %s, %s)",
         #                ('Pyd59B6pARao3ewSFVf2Y1Ov1Co1', 'Plumber', 'null'))
         # conn.commit()
-        cursor.execute("INSERT INTO object_profiles (address, uk_id, object_name, photo_path) VALUES (%s, %s, %s, %s)", ('Object address', 1, 'Object name', 'http://217.25.95.113:8000/static/icons/big/object_main.jpg'))
-        conn.commit()
-        cursor.execute("INSERT INTO apartment_profiles (apartment_name, area, garden, "
-                       "pool, internet_operator, internet_speed, internet_fee, key_holder, object_id) "
-                       "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                       ('122-Floor-2nd floor, Smart, 17', 459.0, True, False, 'mts', 2000, 500.0, 'not', 1))
-        cursor.execute("INSERT INTO apartment_profiles (apartment_name, area, garden, "
-                       "pool, internet_operator, internet_speed, internet_fee, key_holder, object_id) "
-                       "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                       ('122-Floor-2nd floor, Smart, 18', 149.0, False, False, 'Beeline', 1500, 700.0, 'not', 1))
-        cursor.execute("INSERT INTO apartment_profiles (apartment_name, area, garden, "
-                       "pool, internet_operator, internet_speed, internet_fee, key_holder, object_id) "
-                       "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                       ('122-Floor-2nd floor, Smart, 19', 679.0, False, False, 'Megafon', 1889, 800.0, 'not', 1))
-        cursor.execute("INSERT INTO apartment_profiles (apartment_name, area, garden, "
-                       "pool, internet_operator, internet_speed, internet_fee, key_holder, object_id) "
-                       "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                       ('122-Floor-2nd floor, Smart, 120', 913.0, True, True, 'mts', 2500, 300.0, 'not', 1))
-
-        conn.commit()
+        # cursor.execute("INSERT INTO object_profiles (address, uk_id, object_name, photo_path) VALUES (%s, %s, %s, %s)", ('Object address', 1, 'Object name', 'http://217.25.95.113:8000/static/icons/big/object_main.jpg'))
+        # conn.commit()
+        # cursor.execute("INSERT INTO apartment_profiles (apartment_name, area, garden, "
+        #                "pool, internet_operator, internet_speed, internet_fee, key_holder, object_id) "
+        #                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+        #                ('122-Floor-2nd floor, Smart, 17', 459.0, True, False, 'mts', 2000, 500.0, 'not', 1))
+        # cursor.execute("INSERT INTO apartment_profiles (apartment_name, area, garden, "
+        #                "pool, internet_operator, internet_speed, internet_fee, key_holder, object_id) "
+        #                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+        #                ('122-Floor-2nd floor, Smart, 18', 149.0, False, False, 'Beeline', 1500, 700.0, 'not', 1))
+        # cursor.execute("INSERT INTO apartment_profiles (apartment_name, area, garden, "
+        #                "pool, internet_operator, internet_speed, internet_fee, key_holder, object_id) "
+        #                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+        #                ('122-Floor-2nd floor, Smart, 19', 679.0, False, False, 'Megafon', 1889, 800.0, 'not', 1))
+        # cursor.execute("INSERT INTO apartment_profiles (apartment_name, area, garden, "
+        #                "pool, internet_operator, internet_speed, internet_fee, key_holder, object_id) "
+        #                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+        #                ('122-Floor-2nd floor, Smart, 120', 913.0, True, True, 'mts', 2500, 300.0, 'not', 1))
+        #
+        # conn.commit()
         cursor.execute("INSERT INTO contacts (name, phone, email) VALUES (%s, %s, %s, %s)",
                        ('Unified Contact Center', '+7 (495) 150-08-02', 'smart17@sminex.com'))
 
@@ -58,14 +58,14 @@ try:
 
         conn.commit()
 
-        cursor.execute("""INSERT INTO tenant_profiles (uuid, photo_path, active_request, balance, device_token)
-        VALUES (%s, %s, %s, %s, %s)""", ("7A9ipzCF4EUVGZeruLCsOXL35j12", 'null', 0, 0, "dpqmvMwUST-gOWVf3KkG8P:APA91bFd8JXPRWf_VUlI7QFNBBTtdLX0_Tkbls_63csq9pbu-JAHDQBV2d6OTldOatfCAeLPxPDe1-7Byaw1gY9X7NUQG26shoU7WL_mh8HcNRmvAnbAY6b8LgYborGsSJv1NtXxXHmi"))
+        # cursor.execute("""INSERT INTO tenant_profiles (uuid, photo_path, active_request, balance, device_token)
+        # VALUES (%s, %s, %s, %s, %s)""", ("7A9ipzCF4EUVGZeruLCsOXL35j12", 'null', 0, 0, "dpqmvMwUST-gOWVf3KkG8P:APA91bFd8JXPRWf_VUlI7QFNBBTtdLX0_Tkbls_63csq9pbu-JAHDQBV2d6OTldOatfCAeLPxPDe1-7Byaw1gY9X7NUQG26shoU7WL_mh8HcNRmvAnbAY6b8LgYborGsSJv1NtXxXHmi"))
+        #
+        # conn.commit()
 
-        conn.commit()
-
-        cursor.execute("""INSERT INTO tenant_apartments (tenant_id, apartment_id) VALUES (%s, %s)""", (1, 1))
-        cursor.execute("""INSERT INTO tenant_apartments (tenant_id, apartment_id) VALUES (%s, %s)""", (1, 2))
-        conn.commit()
+        # cursor.execute("""INSERT INTO tenant_apartments (tenant_id, apartment_id) VALUES (%s, %s)""", (1, 1))
+        # cursor.execute("""INSERT INTO tenant_apartments (tenant_id, apartment_id) VALUES (%s, %s)""", (1, 2))
+        # conn.commit()
 
         cursor.execute("""INSERT INTO services (name, big_icons_path, mini_icons_path) VALUES (%s, %s, %s)""",
                            ("Cleaning", "http://217.25.95.113:8000/static/icons/big/cleaning.jpg", "http://217.25.95.113:8000/static/icons/mini/cleaning.jpg"))
@@ -114,31 +114,31 @@ try:
 
         conn.commit()
 
-        cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
-                       (1, 1))
-        cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
-                       (1, 2))
-        cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
-                       (1, 3))
-        cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
-                       (1, 4))
-        cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
-                       (2, 1))
-        cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
-                       (2, 2))
-        cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
-                       (2, 3))
-        cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
-                       (2, 4))
+        # cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
+        #                (1, 1))
+        # cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
+        #                (1, 2))
+        # cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
+        #                (1, 3))
+        # cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
+        #                (1, 4))
+        # cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
+        #                (2, 1))
+        # cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
+        #                (2, 2))
+        # cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
+        #                (2, 3))
+        # cursor.execute("""INSERT INTO news_apartments (apartment_id, news_id) VALUES (%s, %s)""",
+        #                (2, 4))
         conn.commit()
         cursor.execute("""INSERT INTO payment_details_uk (recipient_name, inn, kpp, account, bic, correspondent_account, okpo, bank_name, uk_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""",
                        ('Recipient', '5250000000', '525000000', '000000000000000000', '000000000000', '00000000000000000000000000', '0000000000', 'Bank name', 1))
 
-        cursor.execute("""INSERT INTO payment_details_uk (recipient_name, inn, kpp, account, bic, correspondent_account, 
-                okpo, bank_name, uk_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""",
-                       ("Recipient", "5250000000", "525000000", "000000000000000000", "000000000000",
-                        "00000000000000000000000000",
-                        "0000000000", "Bank name", 2))
+        # cursor.execute("""INSERT INTO payment_details_uk (recipient_name, inn, kpp, account, bic, correspondent_account,
+        #         okpo, bank_name, uk_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+        #                ("Recipient", "5250000000", "525000000", "000000000000000000", "000000000000",
+        #                 "00000000000000000000000000",
+        #                 "0000000000", "Bank name", 2))
         conn.commit()
 
         cursor.execute("""INSERT INTO meter_service (name, big_icons_path, mini_icons_path) VALUES (%s, %s, %s)""",
