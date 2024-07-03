a0aca8ee984010ba799bbb76b6315bdfa29d922c
Mekan777-alt <mekan2678@gmail.com>
2024-07-03 16:42:51 +0300

    update S3 upload photo

--- api/routers/S3/main.py
+++ api/routers/S3/main.py
@@ -27,7 +27,7 @@ class S3Client:
 
     async def upload_file(self, photo: UploadFile, user_id: int, role: str):
         async with self.get_client() as s3_client:
-            file_name = f"{role}/{user_id}/photos/{uuid.uuid4()}-{photo.filename}"
+            file_name = f"{role}/{user_id}/{uuid.uuid4()}.{photo.filename.split('.')[-1]}"
             file_content = await photo.read()
             await s3_client.put_object(Body=file_content, Bucket=self.bucket_name, Key=file_name)
 
